fbba5706b6f91fa28197514bb7ab2bfc5f6e5cc5
Habib Ur Rehman Bhatti <101096941+HabibUrRehmanBhatti@users.noreply.github.com>
2022-09-16 01:35:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,151 @@
+navigator.geolocation.getCurrentPosition(function position(csd){
+    let op = csd.coords.latitude;
+    let zp = csd.coords.longitude
+    console.log(op)
+    console.log(zp)
+    console.log(csd)
+  
+  
+  
+  
+    
+  async function ret(){
+    return   new Promise(function(resolve,reject){
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${op}&lon=${zp}&appid=7777de22963359d0d7ee097114ad3a5e&units=metric`)
+      .then((res)=>res.json())
+      .then((res)=>{
+        resolve(  res)
+      })
+      .catch((err)=>{
+        reject(err)
+      })
+    })}
+    async function abc(res){
+      try{
+        let data = await ret()
+        console.log(data.main.temp)
+        console.log(data.weather[0].main)
+        if(data){
+          hed1.innerHTML = data.main.temp
+        hed2.innerHTML = data.weather[0].main
+        hed3.innerHTML = dte
+        if( data.weather[0].main == "Clouds"){
+          imag.innerHTML = `<img src="/cloud(sada).png" id="fix_img" class="imagesssss">` 
+        }
+        else if( data.weather[0].main == "Smoke"){
+          
+          imag.innerHTML = `<img src="/smoke_weather.png" id="fix_img" class="imagesssss">` 
+        }
+        else if( data.weather[0].main == "Clear"){
+          imag.innerHTML = `<img src="/clear.png" id="fix_img" class="imagesssss">` 
+    
+        }
+        else if( data.weather[0].main == "Haze"){
+          imag.innerHTML = `<img src="/smoke_weather.png" id="fix_img" class="imagesssss">` 
+    
+        }
+        else if( data.weather[0].main == "Rain"){
+          imag.innerHTML = `<img src="/cloudrain.png"id="fix_img" class="imagesssss">` 
+    
+        }
+        else if( data.weather[0].main == "Sunny"){
+          imag.innerHTML = `<img src="/sun.png" id="fix_img" class="imagesssss">` 
+    
+        }
+        else if( data.weather[0].main == "Storm"){
+          imag.innerHTML = `<img src="/thundercloud-.webp" id="fix_img" class="imagesssss">` 
+    
+        }
+    
+        }
+        
+        }
+        
+      
+      catch(err){
+        console.log(err)
+      }
+    }
+    
+  abc()
+  })
+  // https://api.openweathermap.org/data/2.5/weather?lat=24.8586567&lon=67.032731&appid=7777de22963359d0d7ee097114ad3a5e&units=metric
+  
+  
+  
+  
+  let dte = new Date().toLocaleString()
+  let locatin_inp = document.getElementById("locatin_inp")
+  let imag = document.getElementById("fix_img_size1")
+  let hed1 = document.getElementById("he_1") 
+  let hed2 = document.getElementById("he_2") 
+  let hed3 = document.getElementById("he_3") 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  async function ret(){
+  return   new Promise(function(resolve,reject){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locatin_inp.value}&appid=fd46bbbc3dc3bc54c806bc8072ffc22b&units=metric`)
+    .then((res)=>res.json())
+    .then((res)=>{
+      resolve(  res)
+    })
+    .catch((err)=>{
+      reject(err)
+    })
+  })}
+  async function abc(res){
+    try{
+      let data = await ret()
+      console.log(data.main.temp)
+      console.log(data.weather[0].main)
+      if(data){
+        hed1.innerHTML = data.main.temp
+      hed2.innerHTML = data.weather[0].main
+      hed3.innerHTML = dte
+  
+      if( data.weather[0].main == "Clouds"){
+        imag.innerHTML = `<img src="/cloud(sada).png" id="fix_img" class="imagesssss">` 
+      }
+      else if( data.weather[0].main == "Smoke"){
+        
+        imag.innerHTML = `<img src="/smoke_weather.png" id="fix_img" class="imagesssss">` 
+      }
+      else if( data.weather[0].main == "Clear"){
+        imag.innerHTML = `<img src="/clear.png" id="fix_img" class="imagesssss">` 
+  
+      }
+      else if( data.weather[0].main == "Haze"){
+        imag.innerHTML = `<img src="/smoke_weather.png" id="fix_img" class="imagesssss">` 
+  
+      }
+      else if( data.weather[0].main == "Rain"){
+        imag.innerHTML = `<img src="/cloudrain.png"id="fix_img" class="imagesssss">` 
+  
+      }
+      else if( data.weather[0].main == "Sunny"){
+        imag.innerHTML = `<img src="/sun.png" id="fix_img" class="imagesssss">` 
+  
+      }
+      else if( data.weather[0].main == "Storm"){
+        imag.innerHTML = `<img src="/thundercloud-.webp" id="fix_img" class="imagesssss">` 
+  
+      }
+  
+      }
+      
+    }
+    catch(err){
+     
+      console.log(err)
+      swal("City not found" )
+    }
+  }
+  
+  
